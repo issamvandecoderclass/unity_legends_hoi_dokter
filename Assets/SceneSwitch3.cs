1d5f343a9ee2a6da7f4f61c959d89d09859220fa
issamvandecoderclass <issam.buamran@coderclass.nl>
2018-03-26 11:57:59 +0200

    Klikbare versie

---
+++ Assets/SceneSwitch3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch3 : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.LoadLevel("scene 4");
+    }
+}
