4b0eb820cefedb2bbc08412c1c7395a499d7bd1d
issamvandecoderclass <issam.buamran@coderclass.nl>
2018-04-08 20:53:47 +0200

    Meerde scenes gemaakt, alles is klaar

---
+++ Assets/SceneSwitch6.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch6 : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.LoadLevel("scene 7");
+    }
+}
---
+++ Assets/SceneSwitch8.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch8 : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.LoadLevel("scene 9");
+    }
+}
